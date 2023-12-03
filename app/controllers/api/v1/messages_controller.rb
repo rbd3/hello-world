ec87ebb2b1fdcb1b5da30423b74a40dbde9a07c8
rbd3 <rabedesanaandry01@gmail.com>
2023-12-03 17:31:24 +0300

    Fix rendering linters problem

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::MessagesController < ApplicationController
-    def random_greeting
-        @random_greeting = Message.order('RANDOM()').first
-        render json: { greeting: @random_greeting.title }
-      end
+  def random_greeting
+    @random_greeting = Message.order('RANDOM()').first
+    render json: { greeting: @random_greeting.title }
+  end
 end
